4abad3b6ee1cb65e0036ddecd374de8bfe098f38
mahfuj <mahfujahmad424@gmail.com>
2024-02-20 01:36:07 +0600

    flkasdf

--- src/component/app/userPage/index.jsx
+++ src/component/app/userPage/index.jsx
@@ -23,20 +23,22 @@ const UserPage = ({ setIsMenuOpen, isMenuOpen, category, setCategory }) => {
           isMenuOpen && "translate-x-0"
         }`}
       >
-        <section className="opacity-90 bg-black flex flex-col absolute left-0 top-0 h-screen p-8 gap-8 z-50 w-full overflow-y-auto">
-          <div>
-            <div>
+        <section className="opacity-90 bg-black flex flex-col absolute left-0 top-0 h-screen gap-8 z-50 w-full overflow-y-auto">
+          <div className="flex">
+            <div className="w-[100%] mx-auto">
               <AuthenticationPage
                 setCategory={setCategory}
                 category={category}
               />
             </div>
-            <div className="flex mt-40 text-white justify-end">
-              <ArrowBackIosIcon
-                onClick={() => setIsMenuOpen(false)}
-                className="cursor-pointer"
-                sx={{ fontSize: "40px" }}
-              />
+            <div className="w-[5%] flex justify-center items-center  h-screen">
+              <div className="text-white">
+                <ArrowBackIosIcon
+                  onClick={() => setIsMenuOpen(false)}
+                  className="cursor-pointer"
+                  sx={{ fontSize: "40px" }}
+                />
+              </div>
             </div>
           </div>
         </section>
