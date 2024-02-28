825028dd5ff5b837cbe9c7e1e04f42e07152d4e8
mahfuj <mahfujahmad424@gmail.com>
2024-02-28 23:52:56 +0600

    nothing to commit

--- src/component/app/userPage/Cart.jsx
+++ src/component/app/userPage/Cart.jsx
@@ -46,6 +46,23 @@ const Cart = () => {
         </div>
         <h1>$50</h1>
       </div>
+      <div className="mt-5 mx-5 text-primary text-xl flex justify-between items-center">
+        <div>
+          <img
+            className="w-20 h-20"
+            src="https://demo.themexpert.com/wordpress/gamez/game-review/wp-content/uploads/sites/3/2016/10/witcher-300x300.jpg"
+            alt=""
+          />
+        </div>
+        <h1>The Witcher 3</h1>
+        <h1>$50</h1>
+        <div className="flex space-x-2">
+            <RemoveIcon sx={{fontSize: "40px"}}/>
+            <div className="w-10 h-10 border border-primary flex items-center justify-center">1</div>
+            <AddIcon sx={{fontSize: "40px"}}/>
+        </div>
+        <h1>$50</h1>
+      </div>
     </div>
   );
 };
