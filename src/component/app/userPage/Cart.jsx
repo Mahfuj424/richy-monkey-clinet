c298fa21b31f1f8d1d85de2e28b7ee445d27a164
mahfuj <mahfujahmad424@gmail.com>
2024-02-28 23:40:39 +0600

    mahfuj

--- src/component/app/userPage/Cart.jsx
+++ src/component/app/userPage/Cart.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-
 const Cart = () => {
   return (
     <div className="max-w-screen-lg mx-auto mt-10">
-      <div>
-        
+      <div className="text-white text-xl font-semibold flex justify-between bg-[#32220c] py-5 px-5 rounded-t-md">
+        <h1>Image</h1>
+        <h1>Product</h1>
+        <h1>Price</h1>
+        <h1>Quantity</h1>
+        <h1>Total</h1>
       </div>
     </div>
   );
