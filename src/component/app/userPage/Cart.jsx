d915776a042f4ba5de5ca61bc5f43e590818dd6c
mahfuj <mahfujahmad424@gmail.com>
2024-03-05 23:18:56 +0600

    mahfuj

--- src/component/app/userPage/Cart.jsx
+++ src/component/app/userPage/Cart.jsx
@@ -15,7 +15,6 @@ const Cart = () => {
         <h1>Total</h1>
         <h1>Action</h1>
       </div>
-      
       <div className="mt-5 mx-5 text-primary text-xl flex justify-between items-center">
         <div>
           <img
